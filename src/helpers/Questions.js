6cfeca35f6e7fe6b5cbbc5999b41356c0431e7f7
Nawaz027 <nawazsiddiqui27@gmail.com>
2022-07-23 12:54:12 +0530

    new files added

---
+++ src/helpers/Questions.js
@@ -0,0 +1,22 @@
+const  Questions = [ 
+    {
+        prompt : "What is my name ?",
+        optionA : "John",
+        optionB: "Nawaz",
+        optionC: "Johnny",
+        optionD: "Nick",
+        answer : "optionB"
+
+    },
+    {
+        prompt : "Which of this is not a programming language ?",
+        optionA : "Phyton",
+        optionB: "Javascript",
+        optionC: "MC-03",
+        optionD: "Java",
+        answer : "optionC"
+
+    }
+];
+
+export default Questions
